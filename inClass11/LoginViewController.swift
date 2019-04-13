7af82b3dc0e2fcb57b23f764e20f6c3a9db7b3c5
Maripau <mhandsch@uncc.edu>
2019-04-13 13:52:25 -0400

    created inClass11 added LoginVC, SignUpVC and UserPhotoVC

---
+++ inClass11/LoginViewController.swift
@@ -0,0 +1,53 @@
+//
+//  LoginViewController.swift
+//  InClass09
+//
+//  Created by Shehab, Mohamed on 3/27/19.
+//  Copyright © 2019 UNCC. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import Firebase
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+    @IBAction func logInClicked(_ sender: Any) {
+        if emailTextField.text == "" || passwordTextField.text == "" {
+            let alert = UIAlertController(title: "Error", message: "You must enter both fields", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "ok", style: .default, handler: nil))
+            self.present(alert, animated: true)
+            
+        } else{
+            Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!){(authResult, error) in
+                
+                if error == nil{
+                    print("success")
+                    AppDelegate.showContacts()
+                } else{
+                    print("error")
+                    let alert = UIAlertController(title: "Error", message: "Incorrect Credentials", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "try again", style: .default, handler: nil))
+                    self.present(alert, animated: true)
+                }
+            }
+        }
+        
+        AppDelegate.showContacts()
+    }
+    
+    @IBAction func createAccountClicked(_ sender: Any) {
+        AppDelegate.showSignUp()
+    }
+    
+}
---
+++ inClass11/UserPhotosViewController.swift
@@ -0,0 +1,38 @@
+//
+//  UserPhotosViewController.swift
+//  inClass11
+//
+//  Created by Maria Handschu on 4/13/19.
+//  Copyright © 2019 Maria Handschu. All rights reserved.
+//
+
+import UIKit
+import FirebaseStorage
+import Firebase
+
+class UserPhotosViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+ 
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "picCell", for: indexPath)
+        
+        return cell
+    }
+    
+
+
+}
